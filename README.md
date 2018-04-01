7a4ddd934c4cc1884e6292e60eb73aa0ef1a4570
ChubbyQuokka <graybad1@icloud.com>
2018-04-01 14:52:30 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Discord Unturned 4.0 Faker
+
+### Installation
+
+1. Open `Unturned 4.0 Faker.sln` with Visual Studio IDE.
+
+2. At the top of your screen, click `Build` then `Build Solution`.
+
+3. Next go into one of the two project folders, `Unturned 4.0 Faker` or `Unturned 4.0 Faker (No Console)`, then `bin/Release/` and drag the `.exe` file somewhere.
+
+4. Drag the `discord-rpc.dll` from `Libraries` into the same directory you put the `.exe`.
+
+5. Run the `.exe` and troll of people. :D
+
+![](https://raw.githubusercontent.com/ChubbyQuokka/Unturned-4.0-Faker/master/img.png)
---
+++ Unturned 4.0 Faker (No Console)/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace testing
+{
+    class Program
+    {
+        public const string DLL = "discord-rpc.dll";
+
+        static void Main(string[] args)
+        {
+            EventHandlers handlers = new EventHandlers();
+            RichPresence presence = new RichPresence();
+
+            Initialize("430037918317281280", ref handlers, true, null);
+
+            presence.state = "Pineridge";
+            presence.details = "In Game";
+            presence.largeImageKey = "unturned";
+
+            UpdatePresence(ref presence);
+
+            Console.ReadLine();
+        }
+
+        public struct EventHandlers { }
+
+        [Serializable]
+        public struct RichPresence
+        {
+            public string state; /* max 128 bytes */
+            public string details; /* max 128 bytes */
+            public long startTimestamp;
+            public long endTimestamp;
+            public string largeImageKey; /* max 32 bytes */
+            public string largeImageText; /* max 128 bytes */
+            public string smallImageKey; /* max 32 bytes */
+            public string smallImageText; /* max 128 bytes */
+            public string partyId; /* max 128 bytes */
+            public int partySize;
+            public int partyMax;
+            public string matchSecret; /* max 128 bytes */
+            public string joinSecret; /* max 128 bytes */
+            public string spectateSecret; /* max 128 bytes */
+            public bool instance;
+        }
+
+        [DllImport(DLL, EntryPoint = "Discord_Initialize", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Initialize(string applicationId, ref EventHandlers handlers, bool autoRegister, string optionalSteamId);
+
+        [DllImport(DLL, EntryPoint = "Discord_UpdatePresence", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void UpdatePresence(ref RichPresence presence);
+    }
+}
